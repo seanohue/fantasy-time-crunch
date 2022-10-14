d6867123574ee96d21df3160287cd9d6ddc48a57
Sean O'Donohue <sodonohue@aurorasolar.com>
2022-10-14 11:49:41 -0400

    run prettier

--- spec/TimeCrunch.spec.js
+++ spec/TimeCrunch.spec.js
@@ -7,22 +7,22 @@ const getExampleTime = require('./_test-helpers');
 
 chai.use(sinonChai);
 
-describe("TimeCrunch", () => {
-  it("should be constructable", () => {
+describe('TimeCrunch', () => {
+  it('should be constructable', () => {
     const timecrunch = new TimeCrunch(getExampleTime());
-    expect(timecrunch).to.be.an("object");
+    expect(timecrunch).to.be.an('object');
   });
 
-  describe("validation", () => {
-    it("should error on missing config", () => {
-      expect(() => new TimeCrunch()).to.throw("No config provided.");
+  describe('validation', () => {
+    it('should error on missing config', () => {
+      expect(() => new TimeCrunch()).to.throw('No config provided.');
     });
   });
 
-  describe("#tick", () => {
-    it("should increment tickUnit w/o args", () => {
+  describe('#tick', () => {
+    it('should increment tickUnit w/o args', () => {
       const timecrunch = new TimeCrunch(getExampleTime());
-      expect(timecrunch.tick).to.be.a("function");
+      expect(timecrunch.tick).to.be.a('function');
 
       timecrunch.tick();
       expect(timecrunch.time.second).to.equal(1);
@@ -30,28 +30,28 @@ describe("TimeCrunch", () => {
       expect(timecrunch.time.second).to.equal(2);
     });
 
-    it("should increment a unit if specified", () => {
+    it('should increment a unit if specified', () => {
       const timecrunch = new TimeCrunch(getExampleTime());
 
-      timecrunch.tick("minute");
+      timecrunch.tick('minute');
       expect(timecrunch.time.minute).to.equal(1);
-      timecrunch.tick("minute");
+      timecrunch.tick('minute');
       expect(timecrunch.time.minute).to.equal(2);
     });
 
-    it("should increment a unit by a specific amount if specified", () => {
+    it('should increment a unit by a specific amount if specified', () => {
       const timecrunch = new TimeCrunch(getExampleTime());
 
-      timecrunch.tick("day", 7);
+      timecrunch.tick('day', 7);
       expect(timecrunch.time.day).to.equal(7);
     });
 
-    it("should call onIncrement if defined", () => {
+    it('should call onIncrement if defined', () => {
       const config = getExampleTime();
-      sinon.spy(config.hour, "onIncrement");
+      sinon.spy(config.hour, 'onIncrement');
       const timecrunch = new TimeCrunch(config);
 
-      timecrunch.tick("hour");
+      timecrunch.tick('hour');
       expect(config.hour.onIncrement).to.have.been.called;
     });
   });
